138b582fd53576df92574a8dcfa0af4be9e4adab
Kristaps Ozols <kristaps.ozols7991@gmail.com>
2022-09-26 13:42:50 +0300

    Corrected the second  homework

--- home-exercises-05-2022/src/main/java/io/codelex/loops/practice/AsciiFigure.java
+++ home-exercises-05-2022/src/main/java/io/codelex/loops/practice/AsciiFigure.java
@@ -3,17 +3,18 @@ package io.codelex.loops.practice;
 public class AsciiFigure {
     public static void main(String[] args) {
         int rows = 7;
-        int charInOneLine = (rows-1)*8;
-        for (int i=0; i<=charInOneLine; i+=8) {
-            for (int j=1;j<=(charInOneLine-i)/2;j++) {
+        int charInOneLine = (rows - 1) * 8;
+        for (int i = 0; i <= charInOneLine; i += 8) {
+            for (int j = 1; j <= (charInOneLine - i) / 2; j++) {
                 System.out.print("/");
-            } for (int k=1;k<=i;k++) {
+            }
+            for (int k = 1; k <= i; k++) {
                 System.out.print("*");
-            } for (int h=1;h<=(charInOneLine-i)/2;h++) {
+            }
+            for (int h = 1; h <= (charInOneLine - i) / 2; h++) {
                 System.out.print("\\");
             }
             System.out.println();
         }
-
     }
 }
--- home-exercises-05-2022/src/main/java/io/codelex/loops/practice/PrintTwoWordsOnSameLine.java
+++ home-exercises-05-2022/src/main/java/io/codelex/loops/practice/PrintTwoWordsOnSameLine.java
@@ -3,6 +3,8 @@ package io.codelex.loops.practice;
 import java.util.Scanner;
 
 public class PrintTwoWordsOnSameLine {
+    public static final int LINE_LENGTH = 30;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter first word:");
@@ -11,12 +13,12 @@ public class PrintTwoWordsOnSameLine {
         System.out.println("Enter second word:");
         String word2 = in.nextLine();
 
-        int charCount = (word1 +word2).length();
-        String dotString = ".".repeat(30-charCount);
+        int charCount = (word1 + word2).length();
+        String dotString = ".".repeat(LINE_LENGTH - charCount);
         System.out.println(word1 + dotString + word2);
 
         System.out.print(word1);
-        for (int i=0; i<30-charCount; i++) {
+        for (int i = 0; i < LINE_LENGTH - charCount; i++) {
             System.out.print(".");
         }
         System.out.print(word2);
